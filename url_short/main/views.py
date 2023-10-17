7b6e96506b0a4e976d4f71692d5ccec90474365d
Ofremu199x <Ofremuendurance89@gmail.com>
2023-10-17 22:09:06 +0300

    added the project

---
+++ url_short/main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import pyshorteners
+import pyperclip
+from django.contrib import messages
+
+# Create your views here.
+
+
+def home(request):
+    
+    
+    
+    
+    return render (request, 'home.html')
+
+
+def result(request):
+    
+    url = request.POST['url']
+    url_shortener = pyshorteners.Shortener()
+    short_url = url_shortener.tinyurl.short(url)
+    st = str(short_url)
+    cp = pyperclip.copy(st)
+    messages.success(request,'The url has been copied to the clipboard') 
+    
+    return render (request, 'result.html',{'st':st })
